07bc1d2de438cce1fe40a28b71371b7408cd7e51
Muthukumar <Muthukumar@LAPTOP-4A09D7UF>
2020-07-31 08:04:15 +0530

    Commit success message

---
+++ com.jenkin.testing/src/test/java/com/jenkin/testing/test1.java
@@ -0,0 +1,12 @@
+package com.jenkin.testing;
+
+import org.testng.annotations.Test;
+
+public class test1 {
+	@Test
+	public void testing1()
+	{
+		System.out.print("Hello testing success");
+	}
+
+}
